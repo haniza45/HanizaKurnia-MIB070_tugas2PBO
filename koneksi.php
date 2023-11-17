cd938beec1800bbb0c3e51eaff67ef50013d61bb
hanizakurnia <hanizaakndp@gmail.com>
2023-11-17 21:51:33 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "crud_gc";
+	$koneksi = mysqli_connect($host,$user,$pass,$db);
+
+	if(!$koneksi) {
+		die("Koneksi dengan database gagal: ".mysql_connect_error());
+	} else {
+		echo "berhasill";
+
+	}
+
+?>
